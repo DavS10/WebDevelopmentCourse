01587813e4850aa2af367fd2581c9a1368c1a2b8
David Santiago <davidsan_34@outlook.com>
2021-09-04 14:21:19 -0500

    Material complementario del curso y practicas

---
+++ semana4y5/AjaxYJson/js/ajax-utils.js
@@ -0,0 +1,45 @@
+(function(global){
+  //Setup a namespace for our utility
+  var ajaxUtils = {};
+  //Returns an HTTP request (petición o solicitud) object
+  function getRequestObject() {
+    if (global.XMLHttpRequest) {
+      return (new XMLHttpRequest());
+    } else {
+      global.alert("The page is not working correctely. Ajax is not supported!");
+      return (null);
+    }
+  }
+
+  //Makes an Ajax GET request to 'requestUrl'
+  ajaxUtils.sendGetRequest = function (requestUrl,responseHandler, isJsonResponse) {
+    var request = getRequestObject();
+    request.onreadystatechange = function(){
+      handleResponse(request, responseHandler, isJsonResponse);
+    };
+    request.open("GET",requestUrl,true);
+    request.send(null); //for POST only
+  };
+
+  //Only calls user provided 'responseHandler'
+  //Function if response is ready and not an error;
+  function handleResponse(request,responseHandler,isJsonResponse) {
+    if((request.readyState == 4) && (request.status == 200)) {
+      //Default to isJsonResponse -> true
+      if(isJsonResponse == undefined){
+        isJsonResponse = true;
+      }
+      if(isJsonResponse){
+        console.log("Response: " + request.response);
+        console.log("----------");
+        console.log("Response text: " + request.responseText);
+        responseHandler(JSON.parse(request.responseText));
+      } else {
+        responseHandler(request.responseText);
+      }
+    }
+  }
+
+  //Expose utility to the global object
+  global.$ajaxUtils = ajaxUtils;
+})(window);
